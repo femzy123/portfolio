a292316e5964efad2a6bd1cbdf0e538f4b1682f6
femzy123 <femzy123@yahoo.com>
2023-06-19 03:08:05 +0100

    Update experience

--- components/works/RpInc.tsx
+++ components/works/RpInc.tsx
@@ -10,7 +10,7 @@ const RpInc = () => {
       transition={{ delay: 0.1 }}
     >
       <h3 className="flex gap-1 font-medium text-xl font-titleFont">
-        Lead Engineer{" "}
+        Software Engineer{" "}
         <span className="text-textGreen tracking-wide">@RP Inc.</span>
       </h3>
       <p className="text-sm mt-1 font-medium text-textDark">
